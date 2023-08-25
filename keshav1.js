2cfb47661f0731c756df5bed7636f48abca15ea0
keshav-chandra <keshavchandra2222@gmail.com>
2023-08-25 16:44:28 +0530

    lets start

---
+++ keshav1.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con =mysql.createConnection({
+ host:"localhost",
+ user :"root",
+ password:""
+});
+
+con.connect(function(err){
+if(err) throw err;
+console.log("connected!");
+con.query("CREATE DATABASE STARONE",function(err ,result){
+if (err) throw err;
+console. log("Database created");
+});
+});
